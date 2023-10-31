1c8940aec85b143263e4f9f4207cffe794f2c219
Rajesh <rajeshsh565@gmail.com>
2023-11-01 04:24:59 +0530

    editting function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import FlipMove from "react-flip-move";
 
 const App = () => {
   const [tasks, setTask] = useState([]);
@@ -7,7 +8,6 @@ const App = () => {
   useEffect(() => {
     document.querySelector(".task-input").value = "";
   }, [tasks]);
-
   const handleTaskAdd = () => {
     const inp = document.querySelector(".task-input").value;
     if (inp === "") {
@@ -15,6 +15,30 @@ const App = () => {
       return;
     }
     setTask([...tasks, inp]);
+  };
+  const handleTaskEdit = (e, i) => {
+    const current = e.currentTarget;
+    const sibling = e.currentTarget.previousSibling;
+    const parent = e.currentTarget.parentElement;
+    if (current.classList.contains("fa-pen-to-square")) {
+      sibling.classList.remove("fa-xmark");
+      current.classList.remove("fa-pen-to-square");
+      current.classList.add("fa-check");
+      parent.style.border = "2px solid black";
+      parent.style.borderRadius = "5px";
+      parent.setAttribute("contentEditable", "true");
+    } else {
+      sibling.classList.add("fa-xmark");
+      current.classList.remove("fa-check");
+      current.classList.add("fa-pen-to-square");
+      parent.setAttribute("contentEditable", "false");
+      parent.style.border = "none";
+      const newTasks = [...tasks];
+      newTasks[i] = parent.innerText;
+      setTask([...newTasks]);
+      // tasks[i] = parent.textContent;
+      // setTask([...tasks]);
+    }
   };
 
   return (
@@ -28,25 +52,33 @@ const App = () => {
           <h3>Tasks</h3>
         </div>
         <div className="task-list">
-          <ul>
-            {tasks.map((task, i) => {
-              return (
-                <li className="task" key={i}>
-                  {task}
-                  <i
-                    className="fas fa-xmark remove"
-                    onClick={() => {
-                      setTask(
-                        tasks.filter((task, index) => {
-                          if (index != i) return task;
-                        })
-                      );
-                    }}
-                  ></i>
-                </li>
-              );
-            })}
-          </ul>
+          <FlipMove>
+            <ul>
+              {tasks.map((task, i) => {
+                return (
+                  <li className="task" key={i}>
+                    {task}
+                    <i
+                      className="fas fa-xmark remove"
+                      onClick={() => {
+                        setTask(
+                          tasks.filter((task, index) => {
+                            if (index != i) return task;
+                          })
+                        );
+                      }}
+                      contentEditable="false"
+                    ></i>
+                    <i
+                      className="fas fa-pen-to-square edit"
+                      onClick={(e) => handleTaskEdit(e, i)}
+                      contentEditable="false"
+                    ></i>
+                  </li>
+                );
+              })}
+            </ul>
+          </FlipMove>
         </div>
       </article>
     </section>
